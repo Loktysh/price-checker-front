c794f34c3a23e105ff14f4e33088aa3fceadde43
vxrossa <ibasalaev@icloud.com>
2022-01-13 23:43:28 +0300

    feat: implement link and h1 tag for name

--- src/components/common/AppHeader/index.tsx
+++ src/components/common/AppHeader/index.tsx
@@ -1,10 +1,13 @@
 import { StyledHeader } from './styled';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
   return (
     <StyledHeader>
-      <h2>PRICE CHECKER</h2>
+      <Link to='/'>
+        <h1>PRICE CHECKER</h1>
+      </Link>
     </StyledHeader>
   );
 };
