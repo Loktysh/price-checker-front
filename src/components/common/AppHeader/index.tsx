d0fb950fcfbb7f4a47e39b6e62866a6eba4b0e53
vxrossa <ibasalaev@icloud.com>
2022-01-15 01:25:02 +0300

    feat: implement basic search field for products

--- src/components/common/AppHeader/index.tsx
+++ src/components/common/AppHeader/index.tsx
@@ -1,13 +1,52 @@
-import { StyledHeader } from './styled';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {
+  StyledHeader,
+  StyledHeaderName,
+  StyledSearchField,
+  StyledSearchButton,
+  StyledSearchInput,
+} from './styled';
+import React, { useState, useEffect } from 'react';
+import { COLOR_GREEN_100 } from '../constants/colors';
+import { API_LINK } from '../constants/index';
+import { useDebounce } from '../../../hooks/';
 
 const AppHeader = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [dropDown, setDropdown] = useState<boolean>(false);
+  const debouncedSearch = useDebounce(inputValue, 500);
+
+  // TODO: move fetch to helpers
+  //? where to put helpers in folder structure
+  const fetchProducts = async (): Promise<any> => {
+    const res: Response = await fetch(API_LINK + inputValue);
+    const data: any = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    if (inputValue.length > 0) {
+      setIsSearching(true);
+      fetchProducts().then(data => {
+        console.log(data);
+        setIsSearching(false);
+      });
+    }
+  }, [debouncedSearch]);
+
   return (
-    <StyledHeader>
-      <Link to='/'>
+    <StyledHeader justify='space-around'>
+      <StyledHeaderName to='/'>
         <h1>PRICE CHECKER</h1>
-      </Link>
+      </StyledHeaderName>
+      <StyledSearchField>
+        <StyledSearchInput
+          type='text'
+          placeholder='Search products'
+          onInput={(e: any) => setInputValue(e.target.value)}
+        />
+        <StyledSearchButton color={COLOR_GREEN_100}>Search!</StyledSearchButton>
+      </StyledSearchField>
     </StyledHeader>
   );
 };
--- src/components/common/AppHeader/styled.ts
+++ src/components/common/AppHeader/styled.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
-import { Flex } from '../../typography';
-import { COLOR_WHITE } from '../constants/colors';
+import { Button, Flex } from '../../typography';
+import { COLOR_WHITE, COLOR_GREEN_100, COLOR_GREEN_300 } from '../constants/colors';
 import { SHADOW_SMALL } from '../constants/shadows';
+import { NavLink } from 'react-router-dom';
+
+// TODO: implement better styling
 
 export const StyledHeader = styled(Flex)`
   background-color: ${COLOR_WHITE};
@@ -9,3 +12,27 @@ export const StyledHeader = styled(Flex)`
   width: 100%;
   box-shadow: ${SHADOW_SMALL};
 `;
+
+export const StyledHeaderName = styled(NavLink)`
+  color: ${COLOR_GREEN_100};
+  text-decoration: none;
+  transition: 0.1s ease;
+  &:hover {
+    color: ${COLOR_GREEN_300};
+    transition: 0.1s ease;
+  }
+`;
+
+export const StyledSearchField = styled(Flex)`
+  height: 3rem;
+`;
+
+export const StyledSearchInput = styled.input`
+  height: 100%;
+`;
+
+export const StyledSearchButton = styled(Button)`
+  height: 100%;
+`;
+
+export const StyledSearchDropdown = styled(Flex)``;
