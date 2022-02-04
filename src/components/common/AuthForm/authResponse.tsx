e4efdd1ccbe427a26f0bee46db49ffc947a85143
vxrossa <ibasalaev@icloud.com>
2022-02-04 09:51:33 +0300

    feat: add types for auth res, add LS token check

--- src/components/common/AuthForm/authResponse.tsx
+++ src/components/common/AuthForm/authResponse.tsx
@@ -1,7 +1,16 @@
 import { API_LINK } from '../constants';
 import { AuthFormParams } from '../types';
 
-export const handleAuthSubmit = async (params: AuthFormParams, type: string) => {
+type AuthorizationResponse = {
+  user: {
+    login: string;
+    user: string;
+  };
+  token: string;
+  renewToken: string;
+};
+
+export const handleAuthSubmit = async (params: AuthFormParams, type: string): Promise<void> => {
   const url = API_LINK + (type === 'login' ? 'login' : 'registration');
   const response = await fetch(url, {
     method: 'POST',
@@ -11,8 +20,16 @@ export const handleAuthSubmit = async (params: AuthFormParams, type: string) => 
     body: JSON.stringify(params),
   });
 
-  const result = await response.json();
+  const result: AuthorizationResponse = await response.json();
 
-  localStorage.setItem('token', JSON.stringify(result.token));
-  localStorage.setItem('renewToken', JSON.stringify(result.renewToken));
+  const savedToken: string | null = localStorage.getItem('token');
+  const savedRenewToken: string | null = localStorage.getItem('renewToken');
+
+  if (!savedToken || savedToken !== result.token) {
+    localStorage.setItem('token', JSON.stringify(result.token));
+  }
+
+  if (!savedRenewToken || savedRenewToken !== result.renewToken) {
+    localStorage.setItem('renewToken', JSON.stringify(result.renewToken));
+  }
 };
