e8edc7632fbcbbf7f49490c578285bcdc464b9ec
ShikiLe <mariajane1897@gmail.com>
2022-02-20 18:09:19 +0300

    feat: add responsiveness to profile page

--- src/components/pages/ProfilePage/ProfilePage.tsx
+++ src/components/pages/ProfilePage/ProfilePage.tsx
@@ -5,6 +5,14 @@ import AppHeader from '../../common/AppHeader';
 import ProfileList from '../../common/ProfileList';
 import { NotLoggedButton, NotLoggedWrapper, StyledNotLogged } from './styled';
 import { Link } from 'react-router-dom';
+import { AppFooter } from '../../common/AppFooter';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const ProfilePage = () => {
   const isLogged = useSelector((state: RootState) => state.login.logged);
@@ -13,7 +21,7 @@ const ProfilePage = () => {
     <ProfileList />
   ) : (
     <NotLoggedWrapper direction='column'>
-      <StyledNotLogged>Please log in to view profile.</StyledNotLogged>
+      <StyledNotLogged>Please login to view profile.</StyledNotLogged>
       <Link to='/login'>
         <NotLoggedButton>Log in</NotLoggedButton>
       </Link>
@@ -21,10 +29,11 @@ const ProfilePage = () => {
   );
 
   return (
-    <>
+    <Container>
       <AppHeader />
       {content}
-    </>
+      <AppFooter />
+    </Container>
   );
 };
 
