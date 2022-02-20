e4a75205ce569175c5b4baa7080aaac75ddbd07e
ShikiLe <mariajane1897@gmail.com>
2022-02-20 19:29:23 +0300

    feat: add redirect after successful auth

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 import styled from 'styled-components';
+import { RootState } from '../../store/store';
 import { AuthForm } from '../common/AuthForm';
 import { handleAuthSubmit } from '../common/AuthForm/authResponse';
 
-// TODO: Implement main page for auth forms
 const AuthPageContainer = styled.div`
   display: flex;
   flex-grow: 1;
 `;
 
 export const LoginPage = () => {
+  const logged = useSelector((state: RootState) => state.login.logged);
   return (
     <AuthPageContainer>
+      {logged && <Navigate to='/profile' />}
       <AuthForm type='login' onAuthSubmit={handleAuthSubmit} />
     </AuthPageContainer>
   );
