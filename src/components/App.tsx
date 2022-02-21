e8906c7529306e6172a64365d77ecaf02665b774
ShikiLe <mariajane1897@gmail.com>
2022-02-21 20:00:06 +0300

    refactor: refactor routing in main page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='/' element={<MainPage />}>
-          <Route path='/' element={<AppLanding />} />
+          <Route index element={<AppLanding />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/settings' element={<SettingsPage />} />
